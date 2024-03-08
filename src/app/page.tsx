d66806fdb90905d08de68e2beac02b3c5a089216
Salman Mahmud <salman.mahmud@cressoftconsultancy.com>
2024-03-08 10:41:47 +0600

    Added header component with auth status

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,3 @@
-import { Button } from "@nextui-org/react";
-import * as action from "@/actions";
-import { auth } from "@/auth";
-import Profile from "@/components/profile";
-
 export default async function Home() {
-  const session = await auth();
-  return (
-    <div>
-      <form action={action.signIn} className="m-5">
-        <Button type="submit">Singin</Button>
-      </form>
-
-      <form action={action.signOut} className="m-5">
-        <Button type="submit">Signout</Button>
-      </form>
-
-      {session?.user ? JSON.stringify(session?.user) : "Signed out"}
-
-      <Profile />
-    </div>
-  );
+  return <div>Home page</div>;
 }
