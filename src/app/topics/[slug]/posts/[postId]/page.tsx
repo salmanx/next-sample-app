635900d83b01e7867dbfaa54f8c402f3a2ec8f3e
Salman Mahmud <salman.mahmud@cressoftconsultancy.com>
2024-03-10 18:30:33 +0600

    Added ability to comment and nested comment functionality

--- src/app/topics/[slug]/posts/[postId]/page.tsx
+++ src/app/topics/[slug]/posts/[postId]/page.tsx
@@ -1,3 +1,28 @@
-export default function PostShowPage() {
-  return <div>Post show page</div>;
+import Link from "next/link";
+import PostShow from "@/components/posts/post-show";
+import CommentList from "@/components/comments/comment-list";
+import CommentCreateForm from "@/components/comments/comment-create-form";
+import { paths } from "@/app/paths";
+import { fetchCommentByPostId } from "@/db/queries/comments";
+
+interface PostShowPageProps {
+  params: {
+    slug: string;
+    postId: string;
+  };
+}
+
+export default async function PostShowPage({ params }: PostShowPageProps) {
+  const { slug, postId } = params;
+
+  return (
+    <div className="space-y-3">
+      <Link className="underline decoration-solid" href={paths.topicShow(slug)}>
+        {"< "}Back to {slug}
+      </Link>
+      <PostShow postId={postId} />
+      <CommentCreateForm postId={postId} startOpen />
+      <CommentList fetchComment={() => fetchCommentByPostId(postId)} />
+    </div>
+  );
 }
